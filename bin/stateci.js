9c2f15776e29bff2825afb08d24afe5d0f64135a
Michael Neil <mneil@mneilsworld.com>
2020-05-29 15:10:13 -0700

    Split up gateway and webhook route with pipeline state machine

--- bin/stateci.js
+++ bin/stateci.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
+if (!process.env.STATECI_ENVIRONMENT) {
+  process.env.STATECI_ENVIRONMENT = 'pr';
+}
+
 const cdk = require('@aws-cdk/core');
 const { BaselineStack } = require('../lib/baseline-stack');
-const { PipelineStack } = require('../lib/pipeline-stack');
+const { PipelineFactory } = require('../lib/pipeline-stack');
 
 const app = new cdk.App();
-const baseline = new BaselineStack(app, 'StateciBaselineStack');
-new PipelineStack(app, 'StateciPipelineFactory', {
+const baseline = new BaselineStack(app, `${process.env.STATECI_ENVIRONMENT}-StateciBaselineStack`);
+
+
+new PipelineFactory(app, `${process.env.STATECI_ENVIRONMENT}-StateciPipelineFactory`, {
   ArtifactBucket: baseline.ArtifactStore,
 });
--- test/unit/baseline.test.js
+++ test/unit/baseline.test.js
@@ -1,11 +1,11 @@
 const { expect, matchTemplate, MatchStyle } = require('@aws-cdk/assert');
 const cdk = require('@aws-cdk/core');
-const Stateci = require('../lib/stateci-stack');
+const Stateci = require('../../lib/baseline-stack');
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Stateci.StateciStack(app, 'MyTestStack');
+    const stack = new Stateci.BaselineStack(app, 'MyTestStack');
     // THEN
     expect(stack).to(matchTemplate({
       "Resources": {}
